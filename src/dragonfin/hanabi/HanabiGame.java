330cabc69fc16f712fff50f6ee21066a379448a6
Jason Long <jason@long.name>
2013-12-15 20:14:54 -0500

    create HanabiGame class

---
+++ src/dragonfin/hanabi/HanabiGame.java
@@ -0,0 +1,69 @@
+package dragonfin.hanabi;
+
+import java.util.*;
+
+public class HanabiGame
+{
+	List<Seat> seats = new ArrayList<Seat>();
+	List<Card> drawPile = new ArrayList<Card>();
+	final static Random R = new Random();
+
+	public void addPlayer(HanabiUser u)
+	{
+		Seat seat = new Seat();
+		seat.user = u;
+		seats.add(seat);
+	}
+
+	public void startGame()
+	{
+		drawPile.clear();
+		for (int i = 0; i < 50; i++) {
+			drawPile.add(new Card(i));
+		}
+		shuffle();
+
+		for (Seat s : seats) {
+			for (int i = 0; i < s.hand.length; i++) {
+				s.hand[i] = drawCard();
+			}
+		}
+	}
+
+	Card drawCard()
+	{
+		return drawPile.remove(drawPile.size()-1);
+	}
+
+	void shuffle()
+	{
+		int len = drawPile.size();
+		for (int i = 0; i < len; i++) {
+			int j = i + R.nextInt(len-i);
+			Card tmp = drawPile.get(i);
+			drawPile.set(i, drawPile.get(j));
+			drawPile.set(j, tmp);
+		}
+	}
+
+	class Seat
+	{
+		HanabiUser user;
+		Card [] hand = new Card[4];
+	}
+
+	class Card
+	{
+		int id;
+		Card(int id)
+		{
+			this.id = id;
+		}
+
+		@Override
+		public boolean equals(Object obj)
+		{
+			return obj instanceof Card && ((Card)obj).id==this.id;
+		}
+	}
+}
--- src/dragonfin/hanabi/HanabiServer.java
+++ src/dragonfin/hanabi/HanabiServer.java
@@ -6,14 +6,36 @@ public class HanabiServer
 {
 	Map<String,HanabiUser> usersByName = new HashMap<String,HanabiUser>();
 	Map<String,HanabiUser> sessions = new HashMap<String,HanabiUser>();
+	Map<String,HanabiGame> games = new HashMap<String,HanabiGame>();
 
 	int nextSessionId = 1;
 
+	HanabiServer()
+	{
+		usersByName.put("Jason", new HanabiUser("Jason"));
+		usersByName.put("Dana", new HanabiUser("Dana"));
+		usersByName.put("Bob", new HanabiUser("Bob"));
+		usersByName.put("Sarah", new HanabiUser("Sarah"));
+
+		HanabiGame g = new HanabiGame();
+		g.addPlayer(usersByName.get("Jason"));
+		g.addPlayer(usersByName.get("Dana"));
+		g.addPlayer(usersByName.get("Bob"));
+		g.addPlayer(usersByName.get("Sarah"));
+		g.startGame();
+
+		games.put("1", g);
+	}
+
 	public String newSession(String userName)
 	{
+		HanabiUser u = usersByName.get(userName);
+		if (u == null) {
+			u = new HanabiUser(userName);
+			usersByName.put(userName, u);
+		}
+
 		String sid = "s"+(nextSessionId++);
-		HanabiUser u = new HanabiUser(userName);
-		usersByName.put(userName, u);
 		sessions.put(sid, u);
 		return sid;
 	}
